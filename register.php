42543880217f99bc2363a8fc06b1400b54dab379
dongww <dongww@gmail.com>
2014-03-06 12:53:25 +0800

    修正css

--- register.php
+++ register.php
@@ -14,26 +14,26 @@
                 <span>注册成功</span>
             </div>
         </div>
-        <main class="span-21 register">
-            <div class="span-14 prepend-1">
-                <form>
-                    <div class="fm-row span-12">
+        <main class="span-16 register">
+            <div class="span-14 prepend-2">
+                <form class="span-14">
+                    <div class="fm-row span-12 last">
                         <label class="span-3"><span class="cl-red">*</span>电子邮箱：</label>
                         <input class="span-9 last" type="text">
                     </div>
-                    <div class="fm-row span-12">
+                    <div class="fm-row span-12 last">
                         <label class="span-3">登录密码：</label>
                         <input class="span-9 last" type="password">
                     </div>
-                    <div class="fm-row span-12">
+                    <div class="fm-row span-12 last">
                         <label class="span-3">确认密码：</label>
                         <input class="span-9 last" type="password">
                     </div>
-                    <div class="fm-row span-12">
-                        <label class="span-3"></label>
+                    <div class="fm-row span-12 last">
+                        <label class="span-3">&nbsp;</label>
                         <button class="btn large span-9 last">同意协议并注册</button>
                     </div>
-                    <div class="fm-row span-12 prepend-3">
+                    <div class="fm-row span-12 prepend-3 last">
                         <a class="cl-blue-light">我已经阅读并同意《》</a>
                     </div>
                 </form>
