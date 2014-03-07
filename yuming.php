2b303859f0a53442f70a4c824ed79111e58df7e7
mayadie <985014704@qq.com>
2014-03-07 15:32:56 +0800

    域名查询页面修改

--- yuming.php
+++ yuming.php
@@ -117,20 +117,73 @@
             <div class="span-16 prepend-1">
                 <div id="tabs">
                     <ul>
-                        <li><a href="#tabs-1">Nunc tincidunt</a></li>
-                        <li><a href="#tabs-2">Proin dolor</a></li>
-                        <li><a href="#tabs-3">Aenean lacinia</a></li>
+                        <li><a href="#tabs-1">域名价格</a></li>
+                        <li><a href="#tabs-2">常见问题</a></li>
                     </ul>
                     <div id="tabs-1">
-                        <p>Proin elit arcu, rutrum commodo, vehicula tempus, commodo a, risus. Curabitur nec arcu. Donec sollicitudin mi sit amet mauris. Nam elementum quam ullamcorper ante. Etiam aliquet massa et lorem. Mauris dapibus lacus auctor risus. Aenean tempor ullamcorper leo. Vivamus sed magna quis ligula eleifend adipiscing. Duis orci. Aliquam sodales tortor vitae ipsum. Aliquam nulla. Duis aliquam molestie erat. Ut et mauris vel pede varius sollicitudin. Sed ut dolor nec orci tincidunt interdum. Phasellus ipsum. Nunc tristique tempus lectus.</p>
+                        <table width="100%" id="mytab"  border="1" class="tab_css_1">
+                            <thead>
+                            <th width="50%">域名注册</th>
+                            <th width="50%">价格</th>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr class="tr_css">
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr  class="tr_css">
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr  class="tr_css">
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr  class="tr_css">
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr class="tr_css">
+                                <td></td>
+                                <td></td>
+                            </tr>
+                            <tr>
+                                <td>&nbsp;</td>
+                                <td>&nbsp;</td>
+                            </tr>
+                        </table>
+
                     </div>
                     <div id="tabs-2">
-                        <p>Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, ut pharetra massa metus id nunc. Duis scelerisque molestie turpis. Sed fringilla, massa eget luctus malesuada, metus eros molestie lectus, ut tempus eros massa ut dolor. Aenean aliquet fringilla sem. Suspendisse sed ligula in ligula suscipit aliquam. Praesent in eros vestibulum mi adipiscing adipiscing. Morbi facilisis. Curabitur ornare consequat nunc. Aenean vel metus. Ut posuere viverra nulla. Aliquam erat volutpat. Pellentesque convallis. Maecenas feugiat, tellus pellentesque pretium posuere, felis lorem euismod felis, eu ornare leo nisi vel felis. Mauris consectetur tortor et purus.</p>
+
+                        <ul>
+                           <li>1、以个人和公司名义不可以申请GOV.CN域名；</li>
+                           <li> 2、域名注册服务机构协助用户注册GOV.CN域名，需同时传真注册申请表及机关法人代码证等相关材料至CNNIC，材料要求：</li>
+                            <li>(1)域名注册申请表注册者必须为政府机构，且注册者名称需与公章一致；</li>
+                            <li>(2)除经过法律法规授权其具有行政管理职能的事业单位可以注册GOV域名外，其他事业单位有使用GOV域名的特定需求，可由上级机关单位注册后授权该事业单位使用，而不能直接注册；</li>
+                            <li>3、请在注册GOV.CN域名前，先从网站上下载并填写《GOV.CN域名及预留域名注册申请表》(点击下载)。将加盖公章后的《GOV.CN域名及预留域名注册申请表》和加盖公章的机关法人代码证</li>
+                        </ul>
+
                     </div>
-                    <div id="tabs-3">
-                        <p>Mauris eleifend est et turpis. Duis id erat. Suspendisse potenti. Aliquam vulputate, pede vel vehicula accumsan, mi neque rutrum erat, eu congue orci lorem eget lorem. Vestibulum non ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce sodales. Quisque eu urna vel enim commodo pellentesque. Praesent eu risus hendrerit ligula tempus pretium. Curabitur lorem enim, pretium nec, feugiat nec, luctus a, lacus.</p>
-                        <p>Duis cursus. Maecenas ligula eros, blandit nec, pharetra at, semper at, magna. Nullam ac lacus. Nulla facilisi. Praesent viverra justo vitae neque. Praesent blandit adipiscing velit. Suspendisse potenti. Donec mattis, pede vel pharetra blandit, magna ligula faucibus eros, id euismod lacus dolor eget odio. Nam scelerisque. Donec non libero sed nulla mattis commodo. Ut sagittis. Donec nisi lectus, feugiat porttitor, tempor ac, tempor vitae, pede. Aenean vehicula velit eu tellus interdum rutrum. Maecenas commodo. Pellentesque nec elit. Fusce in lacus. Vivamus a libero vitae lectus hendrerit hendrerit.</p>
-                    </div>
+
                 </div>
             </div>
         </main>
