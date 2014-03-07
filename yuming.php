06ea1375f6103ad4683322a33e56702d66b77533
mayadie <985014704@qq.com>
2014-03-07 10:10:42 +0800

    域名查询页面

---
+++ yuming.php
@@ -0,0 +1,131 @@
+<?php include 'header.php' ?>
+    <div class="container">
+        <aside class="span-6">
+            <ul class="side-menu-1">
+                <li class="header">产品导航 /Product</li>
+                <li><a>域名注册</a></li>
+                <li><a>虚拟主机</a></li>
+                <li><a>企业邮箱</a></li>
+                <li><a>成品网站</a></li>
+                <li><a>云VPS主机</a></li>
+                <li><a>数据库产品</a></li>
+                <li><a>租用托管</a></li>
+            </ul>
+        </aside>
+
+        <main class="span-18 last">
+        <div class="span-16 prepend-1">
+
+          <div class="chaxun">
+                  <span class="e-chaxun" >英文域名查询</span>
+                  <span class="c-chaxun"> 中文域名查询</span>
+                  <span class="c-chaxun">｜</span>
+                  <span class="c-chaxun">.com
+                      <span class="cl-red jiage">48元</span>
+                  </span>
+                  <span class="c-chaxun">/.net
+                      <span class="cl-red jiage">48元</span>
+                  </span>
+                  <span class="c-chaxun">/.net
+                      <span class="cl-red jiage">48元</span>
+                  </span>
+          </div>
+           <div class="chaxun-2">
+               <form>
+                     <div  class="span-16 chaxun-input">
+                         <input class="span-12 chaxun-input-1" type="text">
+                         <button class="span-3 last tijiao">查询</button>
+                     </div>
+                 <div class="remen span-16">
+                     <div class="span-2 chaxun-remen">热门域名</div>
+                     <div class="span-14 last">
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                         <span  class="span-2 chaxun-1"><input type="checkbox">.com</span>
+                     </div>
+                 </div>
+               </form>
+           </div>
+          <div class="chaxun-jg span-16">
+             <div class="span-16 chaxun-jg-1">
+                 <span class="sousuo icon icon-sousuo span-15">查询结果</span>
+                 <span class="span-15 sousuo-1">
+                     <span  class="span-3">
+                         <input type="checkbox">mayadie.com
+                     </span>
+                     <span class="span-2 cl-green">未注册</span>
+                     <span class="span-10 last">
+                         <select class="span-3 right ">
+                             <option>1年48.00元</option>
+                             <option>2</option>
+                             <option>3</option>
+                             <option>4</option>
+                         </select>
+                     </span>
+                 </span>
+                 <span class="span-15">
+                     <span class="span-15 sousuo-1">
+                     <span  class="span-3">
+                         <input type="checkbox">mayadie.com
+                     </span>
+                     <span class="span-2 cl-green">未注册</span>
+                     <span class="span-10 last">
+                         <select class="span-3 right ">
+                             <option>1年48.00元</option>
+                             <option>2</option>
+                             <option>3</option>
+                             <option>4</option>
+                         </select>
+                     </span>
+                 </span>
+                 </span>
+                <span class="span-15">
+                     <span class="span-15 sousuo-1">
+                     <span  class="span-3">
+                         <input type="checkbox">mayadie.com
+                     </span>
+                     <span class="span-2 cl-red">已注册</span>
+                     <span class="span-3 last right">
+                         <a class="cl-blue-light">查看</a>
+                         <a class="cl-blue-light">前往</a>
+                     </span>
+                 </span>
+                 </span>
+                 <span  class="span-15 gouwuche">
+                      <span  class="span-2 quanxuan">
+                          <input type="checkbox">全选
+                      </span>
+                     <span>
+                          <button class="btn large span-5 last">放入购物车</button>
+                     </span>
+
+                 </span>
+
+             </div>
+          </div>
+        </div>
+        </main>
+    </div>
+<?php include 'footer.php' ?>
