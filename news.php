3be991378135d541daab2616bb98e2b63a843c06
mayadie <985014704@qq.com>
2014-03-08 16:37:05 +0800

    新闻列表页面

---
+++ news.php
@@ -0,0 +1,45 @@
+<?php include 'header.php' ?>
+    <div class="container">
+        <aside class="span-6">
+            <ul class="side-menu-1">
+                <li class="header">新闻公告</li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+            </ul>
+        </aside>
+        <main class="span-18 last">
+            <article class="article news-maim span-18">
+                <div class="news-name span-17">
+                    <span class="news-name-title span-17">
+                       <ul><li>新闻公告</li></ul>
+                    </span>
+                </div>
+
+                <div class="news-body span-17">
+                    <ul>
+                        <li><span class="cl-red">[置顶]</span><a> 我的交易记录怎么查询？</a><span class="news-date cl-gray right">2011/10/10 14:42:05</span></li>
+                        <li><span class="cl-red">[置顶]</span><a> 我的交易记录怎么查询？</a><span class="news-date cl-gray right">2011/10/10 14:42:05</span></li>
+                        <li><a> 我的网站出现违法信息了，但网站不是我做的，我不太会删除</a><span class="news-date cl-gray right">2011/10/10 14:42:05</span></li>
+                        <li><a> 我的网站出现违法信息了，但网站不是我做的，我不太会删除</a><span class="news-date cl-gray right">2011/10/10 14:42:05</span></li>
+                        <li><a> 我的网站出现违法信息了，但网站不是我做的，我不太会删除</a><span class="news-date cl-gray right">2011/10/10 14:42:05</span></li>
+                        <li><a> 我的网站出现违法信息了，但网站不是我做的，我不太会删除</a><span class="news-date cl-gray right">2011/10/10 14:42:05</span></li>
+                        <li><a> 我的网站出现违法信息了，但网站不是我做的，我不太会删除</a><span class="news-date cl-gray right">2011/10/10 14:42:05</span></li>
+                    </ul>
+                </div>
+                <div class="news-page span-13 prepend-2">
+                    <ul id="icons" class="ui-widget ui-helper-clearfix">
+                       <li class="ui-state-default ui-corner-all left" title=".ui-icon-seek-first"><span class="ui-icon ui-icon-seek-first"></span></li>
+                        <li class="ui-state-default ui-corner-all left" title=".ui-icon-carat-1-w"><span class="ui-icon ui-icon-carat-1-w"></span></li>
+                        <li class="new-page-number left">1</li>
+                        <li class="new-page-number left">2</li>
+                        <li class="new-page-number left">3</li>
+                        <li class="ui-state-default ui-corner-all left" title=".ui-icon-carat-1-e"><span class="ui-icon ui-icon-carat-1-e"></span></li>
+                        <li class="ui-state-default ui-corner-all left" title=".ui-icon-seek-end"><span class="ui-icon ui-icon-seek-end"></span></li>
+                    </ul>
+                </div>
+            </article>
+        </main>
+    </div>
+<?php include 'footer.php' ?>
