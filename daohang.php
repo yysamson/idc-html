ff89aa3b2b6b1f9e7aaf9dc18384f09e579c7beb
mayadie <985014704@qq.com>
2014-03-08 15:11:24 +0800

    已做页面导航

---
+++ daohang.php
@@ -0,0 +1,10 @@
+<?php include 'header.php' ?>
+    <div class="container bg-cl-white daohang">
+        <ul class="span-8">
+            <li><a href="news-xiangqing.php" target="_blank">新闻详细页面</a></li>
+            <li><a href="yuming.php" target="_blank">域名查询页面</a></li>
+            <li><a href="register.php" target="_blank">会员登录页面</a></li>
+            <li><a href="product-detal.php" target="_blank">产品展示页面</a></li>
+        </ul>
+    </div>
+<?php include 'footer.php' ?>
