34f862e8fbe5141e27f6ffd24dd91a0e4204610d
mayadie <985014704@qq.com>
2014-03-08 14:38:14 +0800

    产品页面修改

--- product-detal.php
+++ product-detal.php
@@ -1,43 +1,42 @@
 <?php include 'header.php' ?>
-
     <div class="container">
     <main class="span-19 ">
-        <div class="xiangqing span-19">
+    <div class="xiangqing span-18">
+    <div class="xiangqing-name span-18"> 商务II型</div>
+    <div class="xiangqing-content span-18">
+        <div class="span-3"><img src="images/pic.jpg"/></div>
+        <div class="span-15 last xiangqing-shuoming ">
+            <ul>
+                <li> 操作系统： Windows（2008）/Linux（Aliyun Linux 64位）</li>
+                <li> 支持语言： ASP、ASP.NET1.0/2.0/3.5/4.0、PHP4/5、HTML、WAP、Perl5、独立cgi-bin</li>
+                <li> 数据库： ACCESS（50M）、SQL Server2008（150M）、MySQL 5.0.77（150M）</li>
+                <li> 独立网页空间： 1.5G（含数据库空间)</li>
+                <li><a class="cl-blue-light">查看详细></a></li>
+            </ul>
+            <select class="span-4 xiangqing-jiage cl-red">
+                <option>1年48.00元</option>
+                <option>1年48.00元</option>
+                <option>1年48.00元</option>
+                <option>1年48.00元</option>
+            </select>
+            <a class="btn xiangqing-buy">放入购物车</a>
+        </div>
+    </div>
 
-            <span class="xiangqing-name span-18"> 商务II型</span>
-                <span class="xiangqing-content span-18">
-                    <span class="span-3"><img src="images/pic.jpg"/></span>
-                    <span class="span-15 last xiangqing-shuoming ">
-                        <ul>
-                            <li> 操作系统： Windows（2008）/Linux（Aliyun Linux 64位）</li>
-                            <li> 支持语言： ASP、ASP.NET1.0/2.0/3.5/4.0、PHP4/5、HTML、WAP、Perl5、独立cgi-bin</li>
-                            <li> 数据库： ACCESS（50M）、SQL Server2008（150M）、MySQL 5.0.77（150M）</li>
-                            <li> 独立网页空间： 1.5G（含数据库空间)</li>
-                            <li><a class="cl-blue-light">查看详细></a></li>
-                            <select class="span-4 xiangqing-jiage cl-red">
-                                <option>1年48.00元</option>
-                                <option>1年48.00元</option>
-                                <option>1年48.00元</option>
-                                <option>1年48.00元</option>
-                            </select>
-                            <button class="btn span-4 last xiangqing-buy">放入购物车</button>
-                        </ul>
-                    </span>
-                </span>
+    <div class="span-18 xiangqiang-product">
+        <div class="span-18 product-name">
+            <ul class="span-5 bg-cl-gray cl-gray-light">
+                <li>全能入门虚拟主机</li>
+            </ul>
+        </div>
 
-            <div class="span-19 xiangqiang-product">
-                <span class="span-18 product-name">
-                    <ul class="span-5 bg-cl-gray cl-gray-light">
-                        <li>全能入门虚拟主机</li>
-                    </ul>
-                </span>
-
-                <span class="span-19">
-
-                    <span class="span-6 product-border ">
-                        <span class="product-content-name span-6"><ul>
+        <div class="span-18">
+            <div class="span-6 product-border ">
+                        <span class="product-content-name span-6">
+                            <ul>
                                 <li>三木森入门型</li>
-                            </ul></span>
+                            </ul>
+                        </span>
                         <span class="product-content-contene">
                             <ul>
                                 <li>总空间200~300M</li>
@@ -55,74 +54,16 @@
                             <span class="product-price2">元/年</span>
                         </span>
                         <span class="span-4 prepend-1">
-                            <button class="btn span-4 last xiangqing-buy">放入购物车</button>
+                            <a class="btn xiangqing-buy">放入购物车</a>
                         </span>
-                    </span>
-
-                      <span class="span-6 product-border ">
-                        <span class="product-content-name span-6"><ul>
-                                <li>三木森入门型</li>
-                            </ul></span>
-                        <span class="product-content-contene">
-                            <ul>
-                                <li>总空间200~300M</li>
-                                <li> 支持windows/linux</li>
-                                <li> 支持ASP/HTML</li>
-                                <li> 支持Access数据库</li>
-                                <li> 月流量：10G/月</li>
-                                <li> IIS：50个</li>
-                                <li> 机房环境：电信/网通/双线</li>
-                                <li><a class="cl-blue-light">查看详细></a></li>
-                            </ul>
-                        </span>
-                        <span class="span-6 product-price">
-                            <span class="cl-red product-price1">120</span>
-                            <span class="product-price2">元/年</span>
-                        </span>
-                        <span class="span-4 prepend-1">
-                            <button class="btn span-4 last xiangqing-buy">放入购物车</button>
-                        </span>
-                    </span>
-
-                     <span class="span-6 product-border ">
-                        <span class="product-content-name span-6"><ul>
-                                <li>三木森入门型</li>
-                            </ul></span>
-                        <span class="product-content-contene">
-                            <ul>
-                                <li>总空间200~300M</li>
-                                <li> 支持windows/linux</li>
-                                <li> 支持ASP/HTML</li>
-                                <li> 支持Access数据库</li>
-                                <li> 月流量：10G/月</li>
-                                <li> IIS：50个</li>
-                                <li> 机房环境：电信/网通/双线</li>
-                                <li><a class="cl-blue-light">查看详细></a></li>
-                            </ul>
-                        </span>
-                        <span class="span-6 product-price">
-                            <span class="cl-red product-price1">120</span>
-                            <span class="product-price2">元/年</span>
-                        </span>
-                        <span class="span-4 prepend-1">
-                            <button class="btn span-4 last xiangqing-buy">放入购物车</button>
-                        </span>
-                    </span>
             </div>
 
-            <div class="span-19 xiangqiang-product">
-                <span class="span-18 product-name">
-                    <ul class="span-5 bg-cl-gray cl-gray-light">
-                        <li>豪华全能虚拟主机</li>
-                    </ul>
-                </span>
-
-                <span class="span-19">
-
-                    <span class="span-6 product-border ">
-                        <span class="product-content-name span-6"><ul>
+            <div class="span-6 product-border ">
+                        <span class="product-content-name span-6">
+                            <ul>
                                 <li>三木森入门型</li>
-                            </ul></span>
+                            </ul>
+                        </span>
                         <span class="product-content-contene">
                             <ul>
                                 <li>总空间200~300M</li>
@@ -140,14 +81,17 @@
                             <span class="product-price2">元/年</span>
                         </span>
                         <span class="span-4 prepend-1">
-                            <button class="btn span-4 last xiangqing-buy">放入购物车</button>
+                            <a class="btn xiangqing-buy">放入购物车</a>
                         </span>
-                    </span>
+            </div>
 
-                      <span class="span-6 product-border ">
-                        <span class="product-content-name span-6"><ul>
+
+            <div class="span-6 product-border ">
+                        <span class="product-content-name span-6">
+                            <ul>
                                 <li>三木森入门型</li>
-                            </ul></span>
+                            </ul>
+                        </span>
                         <span class="product-content-contene">
                             <ul>
                                 <li>总空间200~300M</li>
@@ -165,14 +109,16 @@
                             <span class="product-price2">元/年</span>
                         </span>
                         <span class="span-4 prepend-1">
-                            <button class="btn span-4 last xiangqing-buy">放入购物车</button>
+                            <a class="btn last xiangqing-buy">放入购物车</a>
                         </span>
-                    </span>
+            </div>
 
-                     <span class="span-6 product-border ">
-                        <span class="product-content-name span-6"><ul>
+            <div class="span-6 product-border last ">
+                        <span class="product-content-name span-6">
+                            <ul>
                                 <li>三木森入门型</li>
-                            </ul></span>
+                            </ul>
+                        </span>
                         <span class="product-content-contene">
                             <ul>
                                 <li>总空间200~300M</li>
@@ -190,12 +136,131 @@
                             <span class="product-price2">元/年</span>
                         </span>
                         <span class="span-4 prepend-1">
-                            <button class="btn span-4 last xiangqing-buy">放入购物车</button>
+                            <a class="btn  xiangqing-buy">放入购物车</a>
                         </span>
-                    </span>
-
             </div>
         </div>
+    </div>
+
+    <div class="span-18 xiangqiang-product">
+        <div class="span-18 product-name">
+            <ul class="span-5 bg-cl-gray cl-gray-light">
+                <li>豪华全能虚拟主机</li>
+            </ul>
+        </div>
+
+        <div class="span-19">
+            <div class="span-6 product-border ">
+                        <span class="product-content-name span-6">
+                            <ul>
+                                <li>三木森入门型</li>
+                            </ul>
+                        </span>
+                        <span class="product-content-contene">
+                            <ul>
+                                <li>总空间200~300M</li>
+                                <li> 支持windows/linux</li>
+                                <li> 支持ASP/HTML</li>
+                                <li> 支持Access数据库</li>
+                                <li> 月流量：10G/月</li>
+                                <li> IIS：50个</li>
+                                <li> 机房环境：电信/网通/双线</li>
+                                <li><a class="cl-blue-light">查看详细></a></li>
+                            </ul>
+                        </span>
+                        <span class="span-6 product-price">
+                            <span class="cl-red product-price1">120</span>
+                            <span class="product-price2">元/年</span>
+                        </span>
+                        <span class="span-4 prepend-1">
+                            <a class="btn  xiangqing-buy">放入购物车</a>
+                        </span>
+            </div>
+
+            <div class="span-6 product-border ">
+                        <span class="product-content-name span-6">
+                            <ul>
+                                <li>三木森入门型</li>
+                            </ul>
+                        </span>
+                        <span class="product-content-contene">
+                            <ul>
+                                <li>总空间200~300M</li>
+                                <li> 支持windows/linux</li>
+                                <li> 支持ASP/HTML</li>
+                                <li> 支持Access数据库</li>
+                                <li> 月流量：10G/月</li>
+                                <li> IIS：50个</li>
+                                <li> 机房环境：电信/网通/双线</li>
+                                <li><a class="cl-blue-light">查看详细></a></li>
+                            </ul>
+                        </span>
+                        <span class="span-6 product-price">
+                            <span class="cl-red product-price1">120</span>
+                            <span class="product-price2">元/年</span>
+                        </span>
+                        <span class="span-4 prepend-1">
+                            <a class="btn  xiangqing-buy">放入购物车</a>
+                        </span>
+            </div>
+
+            <div class="span-6 product-border ">
+                        <span class="product-content-name span-6">
+                            <ul>
+                                <li>三木森入门型</li>
+                            </ul>
+                        </span>
+                        <span class="product-content-contene">
+                            <ul>
+                                <li>总空间200~300M</li>
+                                <li> 支持windows/linux</li>
+                                <li> 支持ASP/HTML</li>
+                                <li> 支持Access数据库</li>
+                                <li> 月流量：10G/月</li>
+                                <li> IIS：50个</li>
+                                <li> 机房环境：电信/网通/双线</li>
+                                <li><a class="cl-blue-light">查看详细></a></li>
+                            </ul>
+                        </span>
+                        <span class="span-6 product-price">
+                            <span class="cl-red product-price1">120</span>
+                            <span class="product-price2">元/年</span>
+                        </span>
+                        <span class="span-4 prepend-1">
+                            <a class="btn  xiangqing-buy">放入购物车</a>
+                        </span>
+            </div>
+
+            <div class="span-6 product-border ">
+                        <span class="product-content-name span-6">
+                            <ul>
+                                <li>三木森入门型</li>
+                            </ul>
+                        </span>
+                        <span class="product-content-contene">
+                            <ul>
+                                <li>总空间200~300M</li>
+                                <li> 支持windows/linux</li>
+                                <li> 支持ASP/HTML</li>
+                                <li> 支持Access数据库</li>
+                                <li> 月流量：10G/月</li>
+                                <li> IIS：50个</li>
+                                <li> 机房环境：电信/网通/双线</li>
+                                <li><a class="cl-blue-light">查看详细></a></li>
+                            </ul>
+                        </span>
+                        <span class="span-6 product-price">
+                            <span class="cl-red product-price1">120</span>
+                            <span class="product-price2">元/年</span>
+                        </span>
+                        <span class="span-4 prepend-1">
+                            <a class="btn  xiangqing-buy">放入购物车</a>
+                        </span>
+            </div>
+        </div>
+    </div>
+
+    </div>
     </main>
 
     <aside class="span-5 last">
@@ -220,6 +285,5 @@
             </ul>
         </div>
     </aside>
-
     </div>
 <?php include 'footer.php' ?>
