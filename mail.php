58c797e7d306c4c1ad6956bf84794f33c0c95141
mayadie <985014704@qq.com>
2014-03-11 13:36:42 +0800

    邮箱产品页面

---
+++ mail.php
@@ -0,0 +1,223 @@
+<?php include 'header.php' ?>
+
+<div class="container">
+
+    <main class="span-19">
+        <div class="xiangqing span-18">
+           <div class="span-18"> <h2 class="news-name-title">三木森全球邮</h2></div>
+            <div class="xiangqing-content span-18">
+                <div class="span-5"><img src="images/pic1.jpg"/></div>
+                <div class="span-13 last xiangqing-shuoming ">
+                    <div class="span-13 mail-shuoming">
+                        <ul>
+                           <li>云架构部署方案稳定、高效</li>
+                           <li>云架构部署方案稳定、高效</li>
+                           <li>云架构部署方案稳定、高效</li>
+                           <li>云架构部署方案稳定、高效</li>
+                           <li>云架构部署方案稳定、高效</li>
+                           <li>云架构部署方案稳定、高效</li>
+                           <li>云架构部署方案稳定、高效</li>
+                           <li>云架构部署方案稳定、高效</li>
+                           <li>云架构部署方案稳定、高效</li
+                        </ul>
+                    </div>
+                    <div class="span-12 mail-title">
+                        <ul>
+                            <li><span class="cl-red">适用范围：</span>注重企业信息化、沟通效率、信息安全的大型企业、集团</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+          <div class="span-18 mail-size">
+              <div class="span-12 mail-size-size">
+                 <div class="span-12 mail-size-rong">
+                      <span class="span-2  mail-size-size1">容量：</span>
+                      <div class="span-7">
+
+                          <div id="green" class="span-7 last"></div>
+                          <div class="span-3 mail-size-size3">1G</div>
+                          <div class="span-4 last mail-size-size4">50G</div>
+                      </div>
+                      <span class="span-2 mail-size-size2">18G</span>
+                  </div>
+                  <div class="span-12 mail-size-rong">
+                      <span class="span-2  mail-size-size1">用户数：</span>
+                      <div class="span-7">
+                          <div id="green" class="span-7 last"></div>
+                          <div class="span-3 mail-size-size3">5个</div>
+                          <div class="span-4 last mail-size-size4">500个</div>
+                      </div>
+                      <span class="span-2 mail-size-size2">185用户</span>
+                      <span class="span-10 prepend-2 mail-xuanze">当前选择：<span class="cl-orange">5</span>用户(<span class="cl-orange">5</span>用户起售，以<span class="cl-orange">5</span>用户为步长增加)<span class="cl-orange">18</span>容量</span>
+                  </div>
+
+                 <div class="span-12" style="padding-top: 20px;">
+                  <span class="span-2  mail-size-size1">域名：</span>
+                  <span class="span-5"><span class="span-1 mail-yuming">@</span> <input class="span-4 last" type="text"></span>
+                  <span class="span-3 last mail-yuming">请输入邮箱绑定域名</span>
+                 </div>
+
+                  <div class="span-12 mail-goumai">
+                      <span class="span-2 mail-size-size1">购买年限：</span>
+                      <span class="span-10 last">
+                         <input class="span-2" type="text"><span class="span-1 mail-size-size1">年</span>
+                      </span>
+                  </div>
+              </div>
+
+              <div class="span-5 last">
+                  <div class="span-5 mail-price">
+                     <span class="span-5"><h2> 费用总计:</h2></span>
+                     <span class="span-5 mail-price1"><span class="cl-orange">2000</span> 元/ <span class="cl-orange">1 </span>年</span>
+                     <span class="span-5 mail-price1">立省<span class="cl-orange"> 0</span> 元</span>
+                  </div>
+                  <div class="span-5 mail-buy"> <button class="btn span-4 last">放入购物车</button></div>
+
+              </div>
+          </div>
+        <div class="span-18 mail-peizhi">
+            <div id="tabs">
+                <ul>
+                    <li><a href="#tabs-1">基本配置</a></li>
+                    <li><a href="#tabs-2">其他说明</a></li>
+                </ul>
+            <div id="tabs-1">
+                <table width="100%" id="mytab" class="mail-peizhi-tr">
+                    <thead>
+                    <td width="30%" class="mail-peizhi-td">30天数据备份与恢复</td>
+                    <td width="80%" class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    <tr>
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr class="tr_css">
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr>
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr class="tr_css">
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr>
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr class="tr_css">
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr>
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr class="tr_css">
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr>
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr class="tr_css">
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                    <tr>
+                        <td class="mail-peizhi-td">30天数据备份与恢复</td>
+                        <td class="mail-peizhi-td1">我的网站出现违法信息了，但网站不是我做的，我不太会删除</td>
+                    </tr>
+                </table>
+            </div>
+            <div id="tabs-2">
+                <ul>
+                    <li>1、以个人和公司名义不可以申请GOV.CN域名；</li>
+                    <li> 2、域名注册服务机构协助用户注册GOV.CN域名，需同时传真注册申请表及机关法人代码证等相关材料至CNNIC，材料要求：</li>
+                    <li>(1)域名注册申请表注册者必须为政府机构，且注册者名称需与公章一致；</li>
+                    <li>(2)除经过法律法规授权其具有行政管理职能的事业单位可以注册GOV域名外，其他事业单位有使用GOV域名的特定需求，可由上级机关单位注册后授权该事业单位使用，而不能直接注册；
+                    </li>
+                    <li>
+                        3、请在注册GOV.CN域名前，先从网站上下载并填写《GOV.CN域名及预留域名注册申请表》(点击下载)。将加盖公章后的《GOV.CN域名及预留域名注册申请表》和加盖公章的机关法人代码证
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+        </div>
+</div>
+
+    </main>
+
+
+
+    <div class="span-5 last right">
+    <aside class="span-5">
+        <div class="aside-help span-4">
+            <ul class="side-menu-2">
+                <li class="header">新闻公告</li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+            </ul>
+        </div>
+    </aside>
+    <aside class="span-5">
+        <div class="aside-help span-4">
+            <ul class="side-menu-2">
+                <li class="header">新闻公告</li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+                <li><a>新闻中心</a></li>
+            </ul>
+        </div>
+    </aside>
+   </div>
+
+
+</div>
+
+    <script>
+        $(function () {
+            $("#tabs").tabs();
+        });
+        function hexFromRGB(r, g, b) {
+            var hex = [
+                r.toString( 16 ),
+                g.toString( 16 ),
+                b.toString( 16 )
+            ];
+            $.each( hex, function( nr, val ) {
+                if ( val.length === 1 ) {
+                    hex[ nr ] = "0" + val;
+                }
+            });
+            return hex.join( "" ).toUpperCase();
+        }
+        function refreshSwatch() {
+            var red = $( "#red" ).slider( "value" ),
+                green = $( "#green" ).slider( "value" ),
+                blue = $( "#blue" ).slider( "value" ),
+                hex = hexFromRGB( red, green, blue );
+            $( "#swatch" ).css( "background-color", "#" + hex );
+        }
+        $(function() {
+            $( "#red, #green, #blue" ).slider({
+                orientation: "horizontal",
+                range: "min",
+                max: 8000,
+                value: 127,
+                slide: refreshSwatch,
+                change: refreshSwatch
+            });
+            $( "#red" ).slider( "value", 255 );
+            $( "#green" ).slider( "value", 140 );
+            $( "#blue" ).slider( "value", 60 );
+        });
+    </script>
+<?php include 'footer.php' ?>
