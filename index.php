89955e98aadee7c4f8226e9aa903e406ecd4ae06
mayadie <985014704@qq.com>
2014-03-18 16:31:28 +0800

    首页

---
+++ index.php
@@ -0,0 +1,184 @@
+<?php include 'header.php' ?>
+<div class="bg-clgreen banner">
+    <div class="container"><img src="images/banner.jpg"/> </div>
+</div>
+<div class="bg-cl-white index-main">
+    <div class="container">
+        <div class="span-16 border">
+            <div class="span-16">
+                <span class="e-chaxun">英文域名查询</span>
+                <span class="c-chaxun"> 中文域名查询</span>
+                <span class="c-chaxun">｜</span>
+                  <span class="c-chaxun">.com
+                      <span class="cl-red jiage">48元</span>
+                  </span>
+                  <span class="c-chaxun">/.net
+                      <span class="cl-red jiage">48元</span>
+                  </span>
+                  <span class="c-chaxun">/.net
+                      <span class="cl-red jiage">48元</span>
+                  </span>
+            </div>
+            <div class="span-16">
+                <form>
+                    <div class="span-15 bg-cl-gray">
+                        <input class="span-11 chaxun-input-1" type="text">
+                        <button class="span-3 last btn tijiao">查询</button>
+                    </div>
+                    <div class="remen span-16">
+                        <div class="span-2 chaxun-remen">热门域名</div>
+                        <div class="span-13 last">
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                            <label class="span-2 chaxun-1"><input type="checkbox">.com</label>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <aside class="span-8 last" >
+            <div class="aside-help span-7">
+                <ul class="side-menu-2">
+                    <li class="header">新闻公告<a class="right news-date1 cl-gray">更多>></a></li>
+                    <li><a>新闻中心新闻中心新闻中心新闻中心</a> <span class="right news-date cl-gray">03-18</span></li>
+                    <li><a>新闻中心新闻中心新闻中心新闻中心</a> <span class="right news-date cl-gray">03-18</span></li>
+                    <li><a>新闻中心新闻中心新闻中心新闻中心</a> <span class="right news-date cl-gray">03-18</span></li>
+                    <li><a>新闻中心新闻中心新闻中心新闻中心</a> <span class="right news-date cl-gray">03-18</span></li>
+                    <li><a>新闻中心新闻中心新闻中心新闻中心</a> <span class="right news-date cl-gray">03-18</span></li>
+                    <li><a>新闻中心新闻中心新闻中心新闻中心</a> <span class="right news-date cl-gray">03-18</span></li>
+                    <li><a>新闻中心新闻中心新闻中心新闻中心</a> <span class="right news-date cl-gray">03-18</span></li>
+
+                </ul>
+            </div>
+        </aside>
+    </div>
+</div>
+
+<div class="container index-main">
+   <div class="span-5 prepend-1 ">
+       <h3 class="icon-cp icon span-4">三木森1G主机</h3>
+       <span class="span-5"><img src="images/cp.jpg"> </span>
+       <h4 class="span-4 cl-red">398元/年</h4>
+       <div class="span-5 index-cp-shuoming">
+           <ul>
+               <li>总空间：200~300M200~300M</li>
+               <li>支持：windows/linux ASP/HTML</li>
+               <li>机房环境：电信/网通/双线</li>
+           </ul>
+       </div>
+       <span class="span-5">
+              <a class="btn xiangqing-buy span-3">立即购买</a>
+        </span>
+   </div>
+    <div class="span-5 prepend-1 ">
+        <h3 class="icon-cp icon span-4">三木森1G主机</h3>
+        <span class="span-5"><img src="images/cp.jpg"> </span>
+        <h4 class="span-4 cl-red">398元/年</h4>
+        <div class="span-5 index-cp-shuoming">
+            <ul>
+                <li>总空间：200~300M200~300M</li>
+                <li>支持：windows/linux ASP/HTML</li>
+                <li>机房环境：电信/网通/双线</li>
+            </ul>
+        </div>
+       <span class="span-5">
+              <a class="btn xiangqing-buy span-3">立即购买</a>
+        </span>
+    </div>
+    <div class="span-5 prepend-1 ">
+        <h3 class="icon-cp icon span-4">三木森1G主机</h3>
+        <span class="span-5"><img src="images/cp.jpg"> </span>
+        <h4 class="span-4 cl-red">398元/年</h4>
+        <div class="span-5 index-cp-shuoming">
+            <ul>
+                <li>总空间：200~300M200~300M</li>
+                <li>支持：windows/linux ASP/HTML</li>
+                <li>机房环境：电信/网通/双线</li>
+            </ul>
+        </div>
+       <span class="span-5">
+              <a class="btn xiangqing-buy span-3">立即购买</a>
+        </span>
+    </div>
+    <div class="span-5 prepend-1 last ">
+        <h3 class="icon-cp icon span-4">三木森1G主机</h3>
+        <span class="span-5"><img src="images/cp.jpg"> </span>
+        <h4 class="span-4 cl-red">398元/年</h4>
+        <div class="span-5 index-cp-shuoming">
+            <ul>
+                <li>总空间：200~300M200~300M</li>
+                <li>支持：windows/linux ASP/HTML</li>
+                <li>机房环境：电信/网通/双线</li>
+            </ul>
+        </div>
+       <span class="span-5">
+              <a class="btn xiangqing-buy span-3">立即购买</a>
+        </span>
+    </div>
+</div>
+
+<div class="bg-cl-white index-main">
+    <div class="container">
+        <div class="link span-24">
+                <div class="left"><a><img src="css/icon/pro.jpg"></a></div>
+                <div class="span-23 link-img">
+                    <img src="images/link.jpg"><img src="images/link.jpg">
+                    <img src="images/link.jpg"><img src="images/link.jpg">
+                    <img src="images/link.jpg"><img src="images/link.jpg">
+                    <img src="images/link.jpg"><img src="images/link.jpg">
+                </div>
+                <div class="right"><a><img src="css/icon/next.jpg"></a></div>
+        </div>
+        <div class="span-24 index-main">
+            <h4 class="span-2 cl-orange">友情合作：</h4>
+            <span class="span-22 index-link last">
+                <ul>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                    <li><a>佛山网站建设</a>｜</li>
+                </ul>
+
+            </span>
+        </div>
+
+    </div>
+</div>
+<?php include 'footer.php' ?>
